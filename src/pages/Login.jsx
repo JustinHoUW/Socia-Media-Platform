2b79d37b26eae8c8b7b55147716833abc66ff5c1
JustinHoUW <justinhouw@gmail.com>
2024-02-24 18:55:24 -0800

    Enhance form validation for Login; refactor styling to seperate Login and Signup Pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { useForm } from "react-hook-form"
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
 import '../Login.css';
-import TextInput from '../components/TextInput';
+import { TextInput } from "../components";
+import CustomButton from '../components/CustonButton';
+import Loading from '../components/Loading';
+import talkingimage from '../images/shutterstock_1859210845.jpeg';
 
 const Login = () => {
   const {
@@ -10,68 +15,105 @@ const Login = () => {
     mode: "onChange",
   });
 
+  const [errMsg, setErrMsg] = useState({ message: "" }); // Initialize errMsg as an object
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const dispatch = useDispatch();
+
+  const onSubmit = async (data) => {
+    // Your submit logic goes here
+  }
+
   return (
     <div className="container">
-      <div className='signin-signup'>
-        <div className="second-container">
-          <div className="form-group has-success">
-            <form action="" className="sign-in-form">
-              <div className='Left-Texts'>
-                <p className="text-primary" style={{ fontSize: '70px', fontWeight: '900' }}>HitmeUp</p>
-                <p className="text-primary-emphasis" style={{ fontSize: '30px', fontWeight: '900' }}>Log in to your account</p>
-                <p className="text-primary-emphasis" style={{ fontSize: '35px', fontWeight: '600', opacity: '0.7' }}>Welcome back</p>
+      <div className="second-container">
+        <div className="form-group has-success">
+          <form action="" className="sign-in-form" onSubmit={handleSubmit(onSubmit)}>
+            {/* LEFT SIDE */}
+            <div className="Edit-Left">
+              <div className="card-wrapper">
+                <div className="card">
+                  <div className="card-body">
+                    <p className="text-primary" style={{ fontSize: '90px', fontWeight: '900' }}>HitmeUp</p>
+                    <p className="text-primary-emphasis" style={{ fontSize: '30px', fontWeight: '900' }}>Log in to your account</p>
+                    <p className="text-primary-emphasis" style={{ fontSize: '35px', fontWeight: '600', opacity: '0.7' }}>Welcome back</p>
+                    <p>Don't have an account? <Link to='/register'>Create Account</Link></p>
+                    <img src={talkingimage} alt="Description of the image" style={{ width: '480px', height: '283.2px', marginTop: '22px', marginLeft: '-9px' }} />
+                  </div>
+                </div>
               </div>
-              <div className="Edit-Signs">
-                <h2 style={{ top: '-15px' }}>Sign in</h2>
-                <div className="form-group">
-                  <label htmlFor="inputEmail" className="col-form-label mt-4">Email Address</label>
-                  <input
-                    type="text"
-                    className={`form-control ${errors.username ? 'is-invalid' : ''}`}
-                    placeholder="Username"
-                    id="inputEmail"
-                    {...register("username", { required: "Username is required" })}
+            </div>
+
+            {/* RIGHT SIDE */}
+            <div className="Edit-Signs">
+              <p className="text-dark" style={{ top: '-15px', fontSize: '45px', fontWeight: '800' }}>Sign In</p>
+              <div className="form-group">
+                <label htmlFor="inputEmail" className="col-form-label mt-4">Email Address</label>
+                <input
+                  style={{ width: '438px' }}
+                  type="text"
+                  className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                  placeholder="Username"
+                  id="inputEmail"
+                  {...register("username", { required: "Username is required" })}
+                />
+                {errors.username && <div className="invalid-feedback">{errors.username.message}</div>}
+              </div>
+              <div className="form-group">
+                <label htmlFor="inputPassword" className="col-form-label mt-4">Password</label>
+                <input
+                  style={{ width: '438px' }}
+                  type="password"
+                  className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                  placeholder="Password"
+                  id="inputPassword"
+                  {...register("password", { required: "Password is required" })}
+                />
+                {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
+              </div>
+              <Link to="/reset-password">Forgot Password ?</Link>
+
+              {errMsg?.message && (
+                <span className={`text-sm ${errMsg?.status === "failed" ? "text-[#f64949fe]" : "text-[#2ba150fe]"}`}>
+                  {errMsg?.message}
+                </span>
+              )}
+              <div className='Login-Button'>
+                {isSubmitting ? (
+                  <Loading />
+                ) : (
+                  <CustomButton
+                    type="submit"
+                    containerStyles="btn btn-outline-dark"
+                    title="Login"
                   />
-                  {errors.username && <div className="invalid-feedback">{errors.username.message}</div>}
-                </div>
-                <div className="form-group">
-                  <label htmlFor="inputPassword" className="col-form-label mt-4">Password</label>
-                  <input
-                    type="password"
-                    className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                    placeholder="Password"
-                    id="inputPassword"
-                    {...register("password", { required: "Password is required" })}
-                  />
-                  {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
-                </div>
-                <button type="submit" className="btn btn-outline-primary">Submit</button>
-                <div className="social-media">
-                  <a href="#" className="social-icon">
-                    <i className="fab fa-facebook"></i>
-                  </a>
-                  <a href="#" className="social-icon">
-                    <i className="fab fa-twitter"></i>
-                  </a>
-                  <a href="#" className="social-icon">
-                    <i className="fab fa-linkedin"></i>
-                  </a>
-                </div>
+                )}
               </div>
-              <button type="submit" className="btn">Sign up</button>
-              <div className="social-media">
-                <a href="#" className="social-icon">
-                  <i className="fab fa-facebook"></i>
-                </a>
-                <a href="#" className="social-icon">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href="#" className="social-icon">
-                  <i className="fab fa-linkedin"></i>
-                </a>
-              </div>
-            </form>
-          </div>
+              <div className='w-full lg:w=1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'></div>
+            </div>
+            <div></div>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <i className="fab fa-facebook"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </div>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <i className="fab fa-facebook"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import '../Login.css';
+import { TextInput } from "../components";
+import CustomButton from '../components/CustonButton';
+import Loading from '../components/Loading';
+import talkingimage from '../images/shutterstock_1859210845.jpeg';
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const {
+    register, handleSubmit, formState: { errors },
+  } = useForm({
+    mode: "onChange",
+  });
 
-export default Register
+  const [errMsg, setErrMsg] = useState({ message: "" }); // Initialize errMsg as an object
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const dispatch = useDispatch();
+
+  const onSubmit = async (data) => {
+    // Your submit logic goes here
+  }
+
+  return (
+    <div className="container">
+      <div className="second-container">
+        <div className="form-group has-success">
+          <form action="" className="sign-in-form" onSubmit={handleSubmit(onSubmit)}>
+            {/* LEFT SIDE */}
+            <div className="Edit-Left">
+              <div className="card-wrapper">
+                <div className="card">
+                  <div className="card-body">
+                    <p className="text-primary" style={{ fontSize: '90px', fontWeight: '900' }}>HitmeUp</p>
+                    <p className="text-primary-emphasis" style={{ fontSize: '30px', fontWeight: '900' }}>Log in to your account</p>
+                    <p className="text-primary-emphasis" style={{ fontSize: '35px', fontWeight: '600', opacity: '0.7' }}>Welcome back</p>
+                    <p>Don't have an account? <Link to='/register'>Create Account</Link></p>
+                    <img src={talkingimage} alt="Description of the image" style={{ width: '480px', height: '283.2px', marginTop: '22px', marginLeft: '-9px' }} />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* RIGHT SIDE */}
+            <div className="Edit-Signs">
+              <p className="text-dark" style={{ top: '-15px', fontSize: '45px', fontWeight: '800' }}>Sign In</p>
+              <div className="form-group">
+                <label htmlFor="inputEmail" className="col-form-label mt-4">Email Address</label>
+                <input
+                  style={{ width: '438px' }}
+                  type="text"
+                  className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                  placeholder="Username"
+                  id="inputEmail"
+                  {...register("username", { required: "Username is required" })}
+                />
+                {errors.username && <div className="invalid-feedback">{errors.username.message}</div>}
+              </div>
+              <div className="form-group">
+                <label htmlFor="inputPassword" className="col-form-label mt-4">Password</label>
+                <input
+                  style={{ width: '438px' }}
+                  type="password"
+                  className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                  placeholder="Password"
+                  id="inputPassword"
+                  {...register("password", { required: "Password is required" })}
+                />
+                {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
+              </div>
+              <Link to="/reset-password">Forgot Password ?</Link>
+
+              {errMsg?.message && (
+                <span className={`text-sm ${errMsg?.status === "failed" ? "text-[#f64949fe]" : "text-[#2ba150fe]"}`}>
+                  {errMsg?.message}
+                </span>
+              )}
+              <div className='Login-Button'>
+                {isSubmitting ? (
+                  <Loading />
+                ) : (
+                  <CustomButton
+                    type="submit"
+                    containerStyles="btn btn-outline-dark"
+                    title="Login"
+                  />
+                )}
+              </div>
+              <div className='w-full lg:w=1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'></div>
+            </div>
+            <div></div>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <i className="fab fa-facebook"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </div>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <i className="fab fa-facebook"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
